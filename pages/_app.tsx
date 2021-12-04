c59e9a54228d988fcabd8f261fba8726e64a72e9
AiRini <tjdwp1999@gmail.com>
2021-12-05 01:02:38 +0900

    feat: mui 테마 provider 정의

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import type { AppProps } from 'next/app';
+import {
+  ThemeProvider,
+  StyledEngineProvider,
+  useMediaQuery,
+} from '@mui/material';
+
+import createThemeByMode from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const isLightMode = useMediaQuery('(prefers-color-scheme: light)');
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={createThemeByMode(isLightMode)}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 }
 
 export default MyApp;
