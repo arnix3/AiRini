c7b03c9486a3603961ef4f4fc4c39a7821a2174a
AiRini <tjdwp1999@gmail.com>
2021-12-05 00:58:37 +0900

    feat: mui 테마 정의

---
+++ theme.ts
@@ -0,0 +1,60 @@
+import { PaletteOptions } from '@mui/material/styles/createPalette';
+import { TypographyOptions } from '@mui/material/styles/createTypography';
+import { createTheme } from '@mui/material';
+
+const defaultPalette: PaletteOptions = {
+  primary: {
+    main: '#79baf2',
+    contrastText: '#161426',
+  },
+  secondary: {
+    main: '#f2a35e',
+    contrastText: '#161426',
+  },
+  info: {
+    main: '#151426',
+  },
+  success: {
+    main: '#5ab281',
+  },
+  warning: {
+    main: '#d7a249',
+  },
+  error: {
+    main: '#f44336',
+  },
+};
+
+const typography: TypographyOptions = {
+  fontFamily: '"Spoqa Han Sans Neo", sans-serif',
+};
+
+export default function createThemeByMode(mode: PaletteOptions['mode']) {
+  const isLightMode = mode === 'light';
+  const palette: PaletteOptions = {
+    mode,
+    ...defaultPalette,
+    background: {
+      paper: '#fff',
+      default: isLightMode ? '#f2f2f2' : '#161426',
+    },
+  };
+
+  return createTheme({
+    palette,
+    typography,
+    components: {
+      MuiTypography: {
+        variants: [
+          {
+            props: { lang: 'en' },
+            style: { fontFamily: 'titillium-web, sans-serif' },
+          },
+        ],
+        defaultProps: {
+          lang: 'ko',
+        },
+      },
+    },
+  });
+}
