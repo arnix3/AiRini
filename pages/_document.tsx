b90069a72a3ee87b27426a6ff751b54ec9576127
AiRini <tjdwp1999@gmail.com>
2021-12-05 02:27:01 +0900

    feat: html meta tag 정의

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,20 @@ class MyDocument extends Document {
       <Html lang={'ko'}>
         <Head>
           <meta title={'author'} content={'AiRini'} />
+          <meta
+            title={'description'}
+            content={'안녕하세요. 프론트엔드 개발자 AiRini입니다.'}
+          />
+          <meta
+            title={'keywords'}
+            content={'AiRini, Front-end developer, Next.js, React.js'}
+          />
+          <meta
+            name={'viewport'}
+            content={'width=device-width, initial-scale=1'}
+          />
+          <meta name={'theme-color'} content={'#79baf2'} />
+          <meta name={'color-scheme'} content={'light dark'} />
         </Head>
         <body>
           <Main />
