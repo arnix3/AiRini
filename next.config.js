879ee6cacf93401650ea372b193165ff08f914ab
AiRini <tjdwp1999@gmail.com>
2021-12-04 23:52:47 +0900

    build: svgr 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: {
+        loader: '@svgr/webpack',
+        options: {
+          svgoConfig: {
+            plugins: [
+              {
+                removeViewBox: false,
+              },
+              {
+                prefixIds: {
+                  prefix: 'svg',
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+    return config;
+  },
+};
