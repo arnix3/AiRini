f654147771a094bbfa4092df2b3fec4089f8f8ed
AiRini <tjdwp1999@gmail.com>
2021-12-05 21:58:09 +0900

    feat: 메인 레이아웃 반응형으로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,13 +19,27 @@ function Home() {
     <Layout>
       <Container>
         <Grid container alignItems={'center'} justifyContent={'center'} gap={4}>
-          <Grid item height={'100vh'}>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              height: '100vh',
+              position: 'relative',
+            }}
+          >
             <Grid
               container
               alignItems={'center'}
+              alignContent={'center'}
               justifyContent={'center'}
-              direction={'column'}
               height={1}
+              gap={4}
+              sx={{
+                flexDirection: {
+                  xs: 'column',
+                  sm: 'row',
+                },
+              }}
             >
               <Grid item className={'selectable__none'}>
                 <Image
@@ -41,21 +55,44 @@ function Home() {
                 />
               </Grid>
               <Grid item>
-                <Typography variant={'h1'} lang={'en'} align={'center'}>
+                <Typography
+                  variant={'h1'}
+                  lang={'en'}
+                  sx={{
+                    textAlign: {
+                      xs: 'center',
+                      sm: 'left',
+                    },
+                  }}
+                >
                   AiRini
                 </Typography>
-                <Typography variant={'subtitle1'} lang={'en'} align={'center'}>
+                <Typography
+                  variant={'body1'}
+                  sx={{
+                    textAlign: {
+                      xs: 'center',
+                      sm: 'left',
+                    },
+                  }}
+                >
                   Front-end web developer
                 </Typography>
               </Grid>
-              <Grid item mt={4}>
-                <Lottie
-                  animationData={scrollAnimation[theme.palette.mode]}
-                  loop
-                  play
-                />
-              </Grid>
             </Grid>
+            <Lottie
+              animationData={scrollAnimation[theme.palette.mode]}
+              loop
+              play
+              css={{
+                width: 'auto',
+                height: 48,
+                position: 'absolute',
+                left: '50%',
+                bottom: '10vh',
+                transform: 'translate(-50%, -50%)',
+              }}
+            />
           </Grid>
           <Grid xs={12} item height={'50vh'}>
             <Typography variant={'h2'} lang={'en'} align={'center'}>
